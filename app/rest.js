b39bac7cb4cb9c4cd879473ee33e2ce2e343a019
Kelvin <kelvin.h.suarez@gmail.com>
2023-01-27 11:59:43 +0100

    modif display, inclu menu

--- app/rest.js
+++ app/rest.js
@@ -1,94 +1,25 @@
+//ANIMATION AVEC SCROLL
 window.addEventListener("scroll", function(){
     var videoBackground = document.getElementById("video-background");
     var y = window.scrollY;
     videoBackground.style.opacity = 1 - (y/150);
 });
 
-
-// window.addEventListener("scroll", function(){
-//     var attentionveamos = document.getElementsByClassName("scrollopcty");
-//     var y = window.scrollY;
-//     for (var i = 0; i < attentionveamos.length; i++) {
-//         attentionveamos[i].style.opacity = 1 - (y/200);
-//     }
-// });
-// var processed = false;
-// window.addEventListener("scroll", function(){
-//     var scrollOpct = document.getElementsByClassName("scrollopcty");
-//     var y = window.scrollY;
-//     for (var i = 0; i < scrollOpct.length; i++) {
-//         var scrollOpct = scrollOpct[i];
-//         var rect = scrollOpct.getBoundingClientRect();
-//         if (rect.top <= y) {
-//             scrollOpct.style.opacity = 1 - (y/200);
-//             processed = true;
-//         }
-//     }
-// });
-
-// var processed = false;
-// window.addEventListener("scroll", function(){
-//     var scrollOpct = document.getElementsByClassName("scrollopcty");
-//     var y = window.scrollY;
-//     for (var i = 0; i < scrollOpct.length; i++) {
-//         var scrollOpct = scrollOpct[i];
-//         var rect = scrollOpct.getBoundingClientRect();
-//         if (rect.top <= y && !processed) {
-//             scrollOpct.style.opacity = 1 - (y/200);
-//             processed = true;
-//         }
-//     }
-// });
-
-
-// var processed = false;
-
-// window.addEventListener("scroll", function(){
-//     var scrollOpct = document.getElementsByClassName("scrollopcty");
-//     var y = window.scrollY;
-//     for (var i = 0; i < scrollOpct.length; i++) {
-//         var scrollOpct = scrollOpct[i];
-//         var rect = scrollOpct.getBoundingClientRect();
-//         if (rect.top <= y && !processed) {
-//             scrollOpct.style.opacity = 1 - (y/300);
-//             processed = true;
-//         }
-//     }
-// }); 
-
-
-// window.addEventListener("scroll", () => {
-//     let y = window.scrollY;
-//     for (let element of this.element) {
-//         element.style.opacity = 1 - (y/200);
-//     }
-// });
-
-// function applyOpacity(attention-veamos) {
-//     let scrollOpacity = new 
-// }
-
-
-
-
-// class ScrollOpacity {
-//     constructor(attentionveamos) {
-//         this.elements = document.getElementsByClassName(attentionveamos);
-//     }
-
-//     applyOpacity() {
-//         window.addEventListener("scroll", () => {
-//             let y = window.scrollY;
-//             for (let element of this.elements) {
-//                 element.style.opacity = 1 - (y/200);
-//             }
-//         });
-//     }
-// }
-
-// function applyOpacity(attentionveamos) {
-//     let scrollOpacity = new ScrollOpacity(attentionveamos);
-//     scrollOpacity.applyOpacity();
-// }
-
-// applyOpacity("opacity-scroll");
+  var buttons = document.querySelectorAll(".maSectionJs .butonConClick");
+  var images = document.querySelectorAll(".maSectionJs .menusVarios");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+      var id = this.getAttribute("data-id");
+      for(var j = 0; j < images.length; j++){
+          if(images[j].getAttribute("data-id") == id){
+              if(images[j].style.display == "block"){
+                  images[j].style.display = "none";
+              }else{
+                  images[j].style.display = "block";
+              }
+          }else{
+              images[j].style.display = "none";
+          }
+      }
+    });
+  }
