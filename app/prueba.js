065342bb56e8f22a2e8fceecb1c3aab64588a4cf
Kelvin <kelvin.h.suarez@gmail.com>
2023-02-02 00:40:07 +0100

    swiper

---
+++ app/prueba.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // opciones aquí
+    autoplay: {
+      delay: 5000,
+    },
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
